ca36862c6904fdd87e5e52206da3b5c128043a6d
BearPolarny <shuller464@gmail.com>
2018-06-26 13:09:09 +0200

    IPCalc v1.2.1

--- IPCalc.py
+++ IPCalc.py
@@ -11,32 +11,64 @@ class IPCalc:
         :param ip: Address IP in '0.0.0.0' format
         :param mask: IP mask in '0.0.0.0' or 'n' format
         """
+
         self.__PRINT_TYPE__ = string_type
         self.ip, self.mask = self.__read_ip__(ip, mask)
         self.network, self.broadcast = self.__calculate_ip_borders__()
         self.min_Host, self.max_Host = self.__calculate_border_hosts__()
         self.hosts = self.__calculate_available_hosts__()
 
-    @staticmethod
-    def __negate_mask__(mask):
-        for i in range(4):
-            mask[i] = 255 - mask[i]
-        return mask
+    def __read_ip__(self, ip, mask):
+
+        try:
+            ip = np.array(ip.split('.')).astype(int)
+            if len(ip) != 4:
+                raise InvalidIPException
+            for octet in ip:
+                if octet > 255 or octet < 0:
+                    raise InvalidIPException
+        except Exception:
+            raise InvalidIPException
+
+        try:
+            mask = np.array(mask.split('.'))
+            if mask.size != 1 or mask.size != 4:
+                raise InvalidMaskException
+            mask = mask.astype(int)
+            if mask.size == 1:
+                mask = mask[0]
+                if mask < 1 or mask > 30:
+                    raise InvalidMaskException
+                mask = self.__calc_mask__(int(mask))
+            else:
+                for octet in mask:
+                    if octet not in self.__viable_octets:
+                        raise InvalidMaskException
+                if mask[3] == 255 or mask[3] == 254 or not self.__is_sorted__(mask):
+                    raise InvalidMaskException
+        except Exception:
+            raise InvalidMaskException
+
+        return ip, mask
 
     def __calculate_ip_borders__(self):
+
         net = []
+
         for oct_ip, oct_mask in zip(self.ip, self.mask):
             net.append(oct_ip & oct_mask)
 
         mask = self.__negate_mask__(self.mask.copy())
 
         bro = []
+
         for oct_ip, oct_mask in zip(self.ip, mask):
             bro.append(oct_ip | oct_mask)
 
         return net, bro
 
     def __calculate_border_hosts__(self):
+
         network = self.network.copy()
         broadcast = self.broadcast.copy()
 
@@ -56,11 +88,42 @@ class IPCalc:
 
         return available_hosts
 
+    def __str__(self):
+
+        if self.__PRINT_TYPE__ == 'dec':
+            string = 'IP: '         + str(self.ip)          + '\n' + \
+                     'Mask: '       + str(self.mask)        + '\n' + \
+                     'Network: '    + str(self.network)     + '\n' + \
+                     'Broadcast: '  + str(self.broadcast)   + '\n' + \
+                     'Min Host: '   + str(self.min_Host)    + '\n' + \
+                     'Max Host: '   + str(self.max_Host)    + '\n' + \
+                     'Hosts: '      + str(self.hosts)       + '\n'
+        else:
+            string = 'Nie chce mi się teraz'
+
+        return string
+
+    @staticmethod
+    def __negate_mask__(mask):
+
+        for i in range(4):
+            mask[i] = 255 - mask[i]
+
+        return mask
+
+    @staticmethod
+    def __is_sorted__(a):
+        for i in range(a.size - 1):
+            if a[i + 1] > a[i]:
+                return False
+        return True
+
     @staticmethod
     def __calc_mask__(num):
 
         mask_d = ['', '', '', '']
         ones = 0
+
         for octet in range(4):
             i = 0
             while ones < num and i < 8:
@@ -76,47 +139,3 @@ class IPCalc:
             mask.append(int(octet, 2))
 
         return np.array(mask)
-
-    def __read_ip__(self, ip, mask):
-        ip = np.array(ip.split('.')).astype(int)
-        if len(ip) != 4:
-            raise InvalidIPException
-        for octet in ip:
-            if octet > 255 or octet < 0:
-                raise InvalidIPException
-        mask = np.array(mask.split('.'))
-        if mask.size != 1 or mask.size != 4:
-            raise InvalidMaskException
-        mask = mask.astype(int)
-        if mask.size == 1:
-            mask = mask[0]
-            if mask < 1 or mask > 30:
-                raise InvalidMaskException
-            mask = self.__calc_mask__(int(mask))
-        else:
-            for octet in mask:
-                if octet not in self.__viable_octets:
-                    raise InvalidMaskException
-            if mask[3] == 255 or mask[3] == 254 or not self.__is_sorted__(mask):
-                raise InvalidMaskException
-        return ip, mask
-
-    def __str__(self):
-        if self.__PRINT_TYPE__ == 'dec':
-            string = 'IP: '         + str(self.ip)          + '\n' + \
-                     'Mask: '       + str(self.mask)        + '\n' + \
-                     'Network: '    + str(self.network)     + '\n' + \
-                     'Broadcast: '  + str(self.broadcast)   + '\n' + \
-                     'Min Host: '   + str(self.min_Host)    + '\n' + \
-                     'Max Host: '   + str(self.max_Host)    + '\n' + \
-                     'Hosts: '      + str(self.hosts)       + '\n'
-        else:
-            string = 'Nie chce mi się teraz'
-        return string
-
-    @staticmethod
-    def __is_sorted__(a):
-        for i in range(a.size - 1):
-            if a[i + 1] > a[i]:
-                return False
-        return True
--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from IPCalc import IPCalc, InvalidMaskException, InvalidIPException
 if __name__ == '__main__':
 
     try:
-        ip = IPCalc(input('Podaj IP: '), input('Podaj Maskę: '), input('"bin" czy "dec"? '))
+        ip = IPCalc(input('Podaj IP: '), input('Podaj Maskę: '))
         print(ip)
     except InvalidMaskException:
         print('Podana maska jest nieprawidłowa')
